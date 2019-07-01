8da22b95cbd775ad2af329e27f0214d2aabf7ac8
XX <xx@XXs-MBP13.local>
2019-07-01 18:53:51 -0400

    dom manipulation is the sh*t. landing page disk moving on click now.

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-
+import staticDisk from '../images/still_rotatingrecord.png';
 import disk from '../images/rotatingrecord.gif';
 import "./Spinner.css";
 
+const movingDisk = (ev) => {
+  const status = ev.target.getAttribute("src");
+  if (status === `${staticDisk}`) {
+    ev.target.setAttribute("src", `${disk}`);
+  } else {
+    ev.target.setAttribute("src", `${staticDisk}`);
+  }
+}
 
 
 export default function Spinner() {
   return (
-  
-      <div className="spinner">
+
+    <div className="spinner">
       <p>Spinner</p>
-          <div className="disk">
-          <img src={disk} alt="Spinning record" />
-          </div>
-
-        <div className=".container">
-        </div>
-
+      <div className="disk">
+        <img src={staticDisk} alt="Spinning record" onClick={movingDisk} />
       </div>
-   
+
+      <div className=".container">
+      </div>
+
+    </div>
+
   )
 }
