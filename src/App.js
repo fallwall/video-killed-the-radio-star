b081f42d7e0f2b84f603f37e315f640061adc671
XX <xx@XXs-MBP13.local>
2019-07-01 23:35:57 -0400

    header margin change

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css';
 
+
 function App() {
   return (
     <>
       <Header />
       <Main />
       <Footer />
+      
+
     </>
   );
 }
